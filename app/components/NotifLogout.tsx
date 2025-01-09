eea539f788163efc8b13cf6ebbae425047c54770
art3m1sz <abhimantraputra@gmail.com>
2025-01-09 21:33:50 +0800

    NotifLogout

---
+++ app/components/NotifLogout.tsx
@@ -0,0 +1,67 @@
+import { Button, Text, TouchableOpacity, View } from "react-native";
+
+export default function NotifLogout() {
+    return (
+        <View
+            style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 20,
+            }}>
+            <View style={{ width: "100%", gap: 14 }}>
+                <Text
+                    style={{ fontSize: 24, fontWeight: "bold", color: "red" }}>
+                    Peringatan!
+                </Text>
+                <Text style={{ fontSize: 20 }}>Yakin Ingin Logout?</Text>
+            </View>
+            <View
+                style={{
+                    width: "100%",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                }}>
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: "white",
+                        width: "40%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        padding: 4,
+                        borderRadius: 8,
+                        borderWidth: 1,
+                        borderColor: "black",
+                    }}>
+                    <Text
+                        style={{
+                            color: "black",
+                            fontSize: 24,
+                            fontWeight: "medium",
+                        }}>
+                        Tidak
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: "red",
+                        width: "40%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        padding: 4,
+                        borderRadius: 8,
+                    }}>
+                    <Text
+                        style={{
+                            color: "white",
+                            fontSize: 24,
+                            fontWeight: "medium",
+                        }}>
+                        Ya
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
